d0548f1b49061de5a70fba274a4e121c29e577dd
mucahitfindik <mucahitfindik97@gmail.com>
2022-07-10 14:50:50 +0200

    added auto-suggestion for formula

--- spa_react/src/components/FormulaForm/FormulaForm.js
+++ spa_react/src/components/FormulaForm/FormulaForm.js
@@ -2,19 +2,20 @@ import { useState, useEffect } from 'react';
 import {getHistoryFormula} from '../../services/HistoryService';
 import {getResultCalculation} from '../../services/CalculateService'
 import Select from 'react-select';
+import AutoSuggest from "react-autosuggest";
 import './FormulaForm.css'
 export const FormulaForm = ({result, setResult, currentCurrency, setCurrentCurrency, currencyList}) => {
     
     const [formulaHistoryList, setFormulaHistoryList] = useState([]);
     const [expression, setExpression] = useState({expression:""});
+    const [suggestions, setSuggestions] = useState([]);
     useEffect(() => {
-        getHistoryFormula()
-        .then((response) => response.formula_history)
-        .then(setFormulaHistoryList)
-        
+        const fetchHistoryFormula = async()=>{
+          const response = await getHistoryFormula();
+          setFormulaHistoryList(response.formula_history)
+        }
+        fetchHistoryFormula();   
       }, [])
-
-    console.log(formulaHistoryList);
     const handleSubmit = (event) =>{
         getResultCalculation(expression)
         .then((response)=> response.result)
@@ -23,7 +24,6 @@ export const FormulaForm = ({result, setResult, currentCurrency, setCurrentCurre
       }
 
       const handleSelect = (data) =>{
-        console.log(data);
         setCurrentCurrency(data);
        
       };
@@ -33,7 +33,29 @@ export const FormulaForm = ({result, setResult, currentCurrency, setCurrentCurre
             return event.target.validity.valid ? {expression:event.target.value} : v
     })
       };*/
+    const getSuggestions = (value) => {
+      return formulaHistoryList.filter(exp =>
+        exp.expression.includes(value.trim())
+      );
+    }
+    const onSuggestionsFetchRequested=( {value}) => {
+      if (!/^[0-9\.\+\-\*\/\(\)]*$/.test(value)){
+        value = expression.expression;
+      }
+      value = value.trim()
+      setExpression({expression:value});
+      setSuggestions(getSuggestions(value))
+    }
+    const getSuggestionValue =(suggestion) =>{
+      setResult(suggestion.result);
+      return suggestion.expression
+    }
+    const onChange = (event, { newValue, method }) => {
+      setExpression({expression:newValue});
+    };
+    
     return(
+      
         <div className="FormulaForm"> 
             <div><h2 style={{ textAlign: "center" }} >Formula Form</h2></div>
             
@@ -51,12 +73,22 @@ export const FormulaForm = ({result, setResult, currentCurrency, setCurrentCurre
             </div>
             &nbsp;
             <form onSubmit={handleSubmit}>
-                <input className = "FormInput"
-                type="text" 
-                pattern="[0-9 \.\+\-\*\/\(\)]*" 
-                value={expression.expression} 
-                onChange={(e) =>setExpression((expression) => (e.target.validity.valid ? {expression:e.target.value} : expression))} 
-                size="40"/> 
+            <AutoSuggest
+              suggestions={suggestions}
+              onSuggestionsClearRequested={() => setSuggestions([])}
+              onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+              getSuggestionValue={getSuggestionValue}
+              renderSuggestion={suggestion => <span>{suggestion.expression}</span>}
+              inputProps={{
+                placeholder: "Please enter your formula",
+                value: expression.expression,
+                onChange: (_, { newValue, method }) => {
+
+                  setExpression({expression:newValue});
+                }
+              }}
+              highlightFirstSuggestion={true}
+            />
                 <p id = "result">={result !== ""&& result}</p>
                 <input type="submit" value="Calculate" />
             </form>
